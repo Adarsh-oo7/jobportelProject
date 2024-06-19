952803a93cddfd0cc0c7ab75970d8917f1cb823b
Adarsh-oo7 <adarshsarachandran@gmail.com>
2024-06-19 19:28:28 +0530

    day one complicted

--- Authentications/urls.py
+++ Authentications/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from  . import views
 urlpatterns = [
-    path('',views.login,name='login')
+    path('',views.login,name='login'),
+    path('reg',views.register,name='register')
 ]
--- Authentications/views.py
+++ Authentications/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request,'jobportal/login.html')
+
+def register(request):
+    return render(request,'jobportal/register.html')
